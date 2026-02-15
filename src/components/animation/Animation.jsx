089a3649fa4bd6629f9ba5480c09040485a3b7b2
moizayub1255 <moizayub401@gmail.com>
2026-02-15 11:37:23 +0500

    animation done

--- src/components/animation/Animation.jsx
+++ src/components/animation/Animation.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import '../../style/animation.css'
+import React from "react";
+import "../../style/animation.css";
 const Animation = () => {
   return (
-    <div>
+    <div className="ani_wrapper">
       <div className="ani_supermain">
-       <div className="color">
-         <div className="ani_main">
-        <img src="https://bugatti.imgix.net/6706954e8f82af44fc03b750/EMBARGO-BUGATTI-World-Premiere-Presskit-Images-20.jpg?auto=format,compress&cs=srgb&sharp=10&w=512&dpr=0.8333333730697632" alt="" className="car_img" />
-      <p className="ani_text"> If comparable, it is no longer Bugatti </p>
-      </div>
-       </div>
+        <div className="ani_main">
+          <p className="ani_text"> If comparable, it is no longer Bugatti </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Animation
+export default Animation;
